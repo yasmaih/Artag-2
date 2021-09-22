0d51c373d01f854e18b58cb2469f00217d1378a4
julienchoro <julienchoro@icloud.com>
2021-09-22 15:09:29 +0200

    Ajout d'une ScrollView a la vue TagScreen

--- Artag/Screens/Tagscreen.swift
+++ Artag/Screens/Tagscreen.swift
@@ -11,65 +11,71 @@ struct TagScreen: View {
     let tag : Tag
     var body: some View{
         
-        VStack(alignment: .leading, spacing: 0.0){
+        ScrollView(){
             Image(tag.image)
                 .resizable()
                 .scaledToFit()
-            Form{
+            
+            VStack {
                 HStack {
                     Image("avion")
-                    Spacer()
                     Text(tag.city)
+                    Spacer()
+
                 }
                 HStack {
                     Image("boite")
+                    Text(tag.category.rawValue)
                     Spacer()
-                Text(tag.category.rawValue)
+
                 }
                 HStack {
-                Image("calendrier")
+                    Image("calendrier")
+                    Text(stringFromDate(d: tag.date))
                     Spacer()
-                Text(stringFromDate(d: tag.date))
-                }
-                HStack {
-                Image("person")
-                    Spacer()
-                    NavigationLink(
-                        destination: ProfilScreens(user : tag.user)
-                            .navigationBarHidden(true),
-                        
-                        label: {
-                            Text(tag.user.name)
-                                
-                        })
-                    
-                    }
-                Text(tag.bio)
-                    Spacer()
+
                 }
                 
-            } .navigationBarTitle(tag.name)
-        }
-       
+                HStack {
+                    NavigationLink(
+                        destination: ProfilScreens(user : tag.user)
+                            .navigationBarHidden(false),
+                        label: {
+                            Image("person")
+                            Text(tag.user.name)
+                            Spacer()
+                            Image(systemName: "greaterthan")
+                        })
+                    
+                }
+                
+                Text(tag.bio)
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+        } .navigationBarTitle(tag.name)
     }
+    
+}
 
-   /* struct FavorisButton : View {
-        @State var isPressed = false
-        var body: some View {
-        ZStack {
-            Image(systemName: "heart.fill")
-                .opacity(isPressed ? 1 : 0)
-                .scaleEffect(isPressed ? 1.0 : 0.1)
-                .animation(.linear)
-            Image(systemName: "heart")
-        }.font(.system(size: 40))
-            .onTapGesture {
-                self.isPressed.toggle()
-        }
-        .foregroundColor(isPressed ? .red : .white)
-        }
-    }
-}*/
+/* struct FavorisButton : View {
+ @State var isPressed = false
+ var body: some View {
+ ZStack {
+ Image(systemName: "heart.fill")
+ .opacity(isPressed ? 1 : 0)
+ .scaleEffect(isPressed ? 1.0 : 0.1)
+ .animation(.linear)
+ Image(systemName: "heart")
+ }.font(.system(size: 40))
+ .onTapGesture {
+ self.isPressed.toggle()
+ }
+ .foregroundColor(isPressed ? .red : .white)
+ }
+ }
+ }*/
 
 struct TagScreen_Previews: PreviewProvider {
     static var previews: some View {
