82841a766d1be759e2f187171f1b2b0e317954c6
julienchoro <julienchoro@icloud.com>
2021-09-22 14:59:54 +0200

    Ajout de la ScrollView dans PublicationTag

--- Artag/Screens/PublicationTag.swift
+++ Artag/Screens/PublicationTag.swift
@@ -18,7 +18,7 @@ struct PublicationTag: View {
     
     @State private var selectedShow : TVShow?
     
-        
+    
     
     var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
@@ -28,95 +28,92 @@ struct PublicationTag: View {
     @State private var dateDajout = Date()
     var body: some View {
         NavigationView{
-        VStack{
-        HStack(spacing: 20) {
-            Button(action: {
-                selectedShow = TVShow(name: "vous allez être redireger vers l'accueil")
-            }, label: {
-                ZStack{
-                    Rectangle()
-                        .frame(width: 100, height: 40)
-                        .cornerRadius(12)
-                        .foregroundColor(.red)
-                    HStack{
-                        Image(systemName: "xmark.circle")
-                        Text("Annuler")
-                    }.foregroundColor(.white)
-                }.padding(4)
-            })
-            Spacer()
-            Button(action: {
-                selectedShow = TVShow(name: "Vous allez être redireger vers votre Tag")
-            }, label: {
-                ZStack{
-                    Rectangle()
-                        .frame(width: 100, height: 40)
-                        .cornerRadius(12)
-                        .foregroundColor(.green)
-                    HStack{
-                        Image(systemName: "checkmark")
-                        Text("Publier")
-                    }.foregroundColor(.white)
-                }.padding(4)
-            })
-
-        }
-        .alert(item: $selectedShow) { show in
-            Alert(title: Text(show.name), message: Text(" "), dismissButton: .cancel())
-        }
-            Text("Publiez un tag")
-                .font(.largeTitle)
-                .bold()
-            HStack{
-                Image(systemName: "photo")
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(10)
-                    .padding()
-                Spacer()
-            }
-            Form(){
-                
-                HStack{
+            ScrollView {
+                HStack(spacing: 20) {
+                    Button(action: {
+                        selectedShow = TVShow(name: "vous allez être redireger vers l'accueil")
+                    }, label: {
+                        ZStack{
+                            Rectangle()
+                                .frame(width: 100, height: 40)
+                                .cornerRadius(12)
+                                .foregroundColor(.red)
+                            HStack{
+                                Image(systemName: "xmark.circle")
+                                Text("Annuler")
+                            }.foregroundColor(.white)
+                        }.padding(4)
+                    })
+                    Spacer()
+                    Button(action: {
+                        selectedShow = TVShow(name: "Vous allez être redireger vers votre Tag")
+                    }, label: {
+                        ZStack{
+                            Rectangle()
+                                .frame(width: 100, height: 40)
+                                .cornerRadius(12)
+                                .foregroundColor(.green)
+                            HStack{
+                                Image(systemName: "checkmark")
+                                Text("Publier")
+                            }.foregroundColor(.white)
+                        }.padding(4)
+                    })
                     
-                    Text("Nom du lieu :")
-                    TextField("nom du lieu", text: $name)
+                }
+                .alert(item: $selectedShow) { show in
+                    Alert(title: Text(show.name), message: Text(" "), dismissButton: .cancel())
+                }
+                Text("Publiez un tag")
+                    .font(.largeTitle)
+                    .bold()
+                HStack{
+                    Image(systemName: "photo")
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(10)
+                        .padding()
                     Spacer()
                 }
-                
-                VStack(alignment: .leading) {
-                    Text("Date d'exposition :")
+                VStack(){
+                    
                     HStack{
                         
-                        DatePicker(selection: $dateDajout, in: ...Date(), displayedComponents: .date) {}.scaledToFit()
-                        Text("au")
-                        DatePicker(selection: $dateDajout, in: ...Date(), displayedComponents: .date) {}.scaledToFit()
+                        Text("Nom du lieu :")
+                        TextField("nom du lieu", text: $name)
+                        Spacer()
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("Date d'exposition :")
+                        HStack{
+                            
+                            DatePicker(selection: $dateDajout, in: ...Date(), displayedComponents: .date) {}.scaledToFit()
+                            Text("au")
+                            DatePicker(selection: $dateDajout, in: ...Date(), displayedComponents: .date) {}.scaledToFit()
+                        }
+                    }
+                    
+                    HStack{
+                        Text("Catégorie :")
+                        TextField("catégorie",text: $category)
+                    }
+                    
+                    HStack{
+                        Text("Ville :")
+                        TextField("ville",text: $city)
+                    }
+                    
+                    HStack{
+                        Text("Commentaire :")
+                        TextField("commentaire", text: $bio)
                     }
                 }
+                .padding(.horizontal)
                 
-                HStack{
-                    Text("Catégorie :")
-                    TextField("catégorie",text: $category)
-                }
-                
-                HStack{
-                    Text("Ville :")
-                    TextField("ville",text: $city)
-                }
-                
-                HStack{
-                    Text("Commentaire :")
-                    TextField("commentaire", text: $bio)
-                }
-            }
-            
-            
-                
-        }.navigationBarHidden(true)
-        
+            }.navigationBarHidden(true)
+        }
     }
-    }
-    
 }
 
 
